9cad60368ea610a9ebb5cad6b853bcb05ac1faa1
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-11-22 22:06:13 +0100

    Add: added the opay component.

---
+++ opay/Opay.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Opay = () => {
+  return (
+    <View>
+      <Text>Opay</Text>
+    </View>
+  );
+};
+
+export default Opay;
+
+const styles = StyleSheet.create({});
