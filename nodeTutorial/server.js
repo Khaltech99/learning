020c8d3c62670df31cc2c0608500d9dc218772b8
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-11-26 23:06:22 +0100

    Fix: Update server response message and logging format in server.js

--- nodeTutorial/server.js
+++ nodeTutorial/server.js
@@ -1,11 +1,13 @@
 import http from "http";
 
 const PORT = 5000;
+
 const server = http.createServer((req, res) => {
-  res.write("hello i just started node");
+  res.writeHead(200, { "content-type": "text/html" });
+  res.write("<h1>started nodejs</h1>");
   res.end();
 });
 
 server.listen(PORT, () => {
-  console.log(`listening to port number:${PORT}`);
+  console.log(`listening on port ${PORT}`);
 });
