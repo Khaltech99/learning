d731ee570e4ae0857edfa9ab97a53ac8a50966af
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-11-27 17:33:12 +0100

    Fix: Refactor server.js to handle GET requests and improve error handling

--- nodeTutorial/server.js
+++ nodeTutorial/server.js
@@ -3,9 +3,28 @@ import http from "http";
 const PORT = 5000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write("<h1>started nodejs</h1>");
-  res.end();
+  try {
+    // if get request
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end("<h1>this is homepage</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end("<h1>This is the about page</h1>");
+      } else {
+        res.writeHead(404, { "content-type": "text/html" });
+        res.end("<h1>invalid page</h1>");
+      }
+    } else {
+      throw new Error("method not allowed");
+    }
+    // if not get request
+  } catch (error) {
+    res.writeHead(500, { "content-type": "text/plain" });
+
+    res.end("server error");
+  }
 });
 
 server.listen(PORT, () => {
