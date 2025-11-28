32ebfdfef76014dde0afd5914c7f38839b6b8f8f
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-11-28 23:50:00 +0100

    Refactor: Update server3.js to use promises for file reading

--- nodeTutorial/server3.js
+++ nodeTutorial/server3.js
@@ -1,6 +1,18 @@
-import { readFile } from "fs";
+// import { readFile } from "fs";
 
-readFile("text.txt", "utf-8", (error, data) => {
-  if (error) throw Error;
-  console.log(data);
-});
+// readFile("text.txt", "utf8", (error, data) => {
+//   if (error) throw Error;
+//   console.log(data);
+// });
+
+import { readFile } from "fs/promises";
+
+const run = async () => {
+  try {
+    const data = await readFile("text.txt", "utf8");
+    console.log(data);
+  } catch (error) {
+    throw new Error();
+  }
+};
+run();
