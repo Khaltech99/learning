d024089fef5dd4fe1566bbc2f702a9092e16423b
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-12-04 13:09:26 +0100

    Add: Added loginController.

---
+++ jwtpractice/controllers/loginController.js
@@ -0,0 +1,37 @@
+import { user } from "../users.js";
+import bcrypt from "bcrypt";
+import crypto from "crypto";
+
+import jwt from "jsonwebtoken";
+
+const secret = process.env.SECRET;
+
+const loginController = async (req, res, next) => {
+  if (!req.body) {
+    return res.status(400).json({ message: "invalid body" });
+  }
+
+  const { email, password } = req.body;
+
+  const foundUser = user.find((u) => u.email === email);
+
+  if (!foundUser) {
+    return res.status(400).json({ message: "user not found" });
+  }
+
+  const correctPassword = await bcrypt.compare(password, foundUser.password);
+
+  if (!correctPassword) {
+    return res.status(400).json({ message: "incorrect password" });
+  }
+
+  const sentToken = jwt.sign({ email: foundUser.email }, secret, {
+    expiresIn: "2h",
+  });
+
+  return res
+    .status(200)
+    .json({ message: "user logged in successfully", token: sentToken });
+};
+
+export default loginController;
