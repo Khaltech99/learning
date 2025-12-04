350fe501bdfe55cc94b8a680a05960c0d9191586
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-12-04 13:08:32 +0100

    Fix: Fixed server.js

--- jwtpractice/server.js
+++ jwtpractice/server.js
@@ -1,13 +1,14 @@
 import express from "express";
 import cors from "cors";
 import router from "./auth.js";
-import errorHandler from "./errorHandler.js";
+import errorHandler from "./middlewares/errorHandler.js";
 
 const app = express();
 
 // middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/user/auth", router);
 
