0b9d8d80b56929869879480f742859a338af1cec
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-12-04 13:10:54 +0100

    ...

--- jwtpractice/errorHandler.js
+++
@@ -1,8 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  if (err) {
-    return res.status(err.status || 404).json({ msg: "invalid route" });
-  }
-  next();
-};
-
-export default errorHandler;
