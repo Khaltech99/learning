d36c49b122848f34e4affcbbf841a05a577e9ef0
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-12-04 07:17:02 +0100

    Add: Port initialization

--- jwtpractice/server.js
+++ jwtpractice/server.js
@@ -1,3 +1,10 @@
 import express from "express";
 
 const app = express();
+
+// port and port listeners
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`listening to port:${port}`);
+});
