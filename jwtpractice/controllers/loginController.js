8de56474ce577fc11d8f3bf9b7b95de0a2bc6034
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-12-10 14:05:52 +0100

    FIx: Database implementation.

--- jwtpractice/controllers/loginController.js
+++ jwtpractice/controllers/loginController.js
@@ -1,36 +1,38 @@
-import { user } from "../users.js";
 import bcrypt from "bcrypt";
-
+import User from "../model/userModel.js";
 import jwt from "jsonwebtoken";
 
 const secret = process.env.SECRET;
 
 const loginController = async (req, res, next) => {
-  if (!req.body) {
-    return res.status(400).json({ message: "invalid body" });
-  }
-
   const { email, password } = req.body;
+  try {
+    if (!email || !password)
+      return res.status(400).json({ message: "No email, or Password" });
 
-  const foundUser = user.find((u) => u.email === email);
+    const foundUser = await User.findOne({ email });
 
-  if (!foundUser) {
-    return res.status(400).json({ message: "user not found" });
+    if (!foundUser) return res.status(400).json({ message: "No user found" });
+
+    const correctPassword = await bcrypt.compare(password, foundUser.password);
+
+    if (!correctPassword)
+      return res.status(400).json({ message: "incorrect email or password" });
+
+    const sentToken = jwt.sign(
+      { id: foundUser._id, email: foundUser.email },
+      secret,
+      {
+        expiresIn: "2h",
+      }
+    );
+
+    return res
+      .status(200)
+      .json({ message: "user logged in successfully", token: sentToken });
+  } catch (err) {
+    res.status(500).json({ message: "Unable to login user", err });
   }
-
-  const correctPassword = await bcrypt.compare(password, foundUser.password);
-
-  if (!correctPassword) {
-    return res.status(400).json({ message: "incorrect password" });
-  }
-
-  const sentToken = jwt.sign({ email: foundUser.email }, secret, {
-    expiresIn: "2h",
-  });
-
-  return res
-    .status(200)
-    .json({ message: "user logged in successfully", token: sentToken });
 };
 
 // will get back to this
