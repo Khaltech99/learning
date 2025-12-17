f2a5bb5fdc97906bbe7193d515ea7c719b3832d0
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-12-17 23:04:44 +0100

    Add: routes

---
+++ todo/backend/routes/todo.route.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import {
+  createTodo,
+  deleteTodo,
+  editTodo,
+  getTodos,
+} from "../controllers/todo.controllers.js";
+
+const router = Router();
+
+// get todos route
+router.get("/todos", getTodos);
+
+//  create todo route
+router.post("/todos/create", createTodo);
+
+// delete todo route
+router.delete("/todos/delete/:id", deleteTodo);
+
+// edit todo route
+router.put("/todos/edit/:id", editTodo);
+
+export default router;
