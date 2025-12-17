c50dc99dfee7b2fa41effbb642edba1687652611
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-12-17 23:26:34 +0100

    Add: Added the authrouter

--- todo/backend/app.js
+++ todo/backend/app.js
@@ -1,5 +1,6 @@
 import express, { urlencoded } from "express";
-import router from "./routes/todo.route.js";
+import router from "./routes/todo_route/todo.route.js";
+import authRouter from "./routes/auth_route/auth.route.js";
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", router);
+app.use("/api/auth", authRouter);
 
 export default app;
