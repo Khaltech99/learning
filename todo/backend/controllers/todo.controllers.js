6ba9f9f18e566d96fc577b524501a5f03fc3f1fd
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-12-17 23:04:33 +0100

    Add: controllers

---
+++ todo/backend/controllers/todo.controllers.js
@@ -0,0 +1,11 @@
+// get todos
+export const getTodos = (req, res) => {};
+
+// create todos
+export const createTodo = (req, res) => {};
+
+// delete todos
+export const deleteTodo = (req, res) => {};
+
+// edit todos
+export const editTodo = (req, res) => {};
