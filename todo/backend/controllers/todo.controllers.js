87b76e177e9197ba7266c87157ed42a325ad2c60
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-12-21 15:40:20 +0100

    Add: Added protected route so that only authorized user can create todo.

--- todo/backend/controllers/todo.controllers.js
+++ todo/backend/controllers/todo.controllers.js
@@ -1,17 +1,33 @@
-import { createTodoServices } from "../services/todo.services.js";
+import Todo from "../models/todo.model.js";
+import {
+  createTodoServices,
+  deleteTodoServices,
+  getTodoServices,
+} from "../services/todo.services.js";
 
 // get todos
 export const getTodos = async (req, res) => {
-  res.status(200).json({ message: "success", user: req.user });
+  try {
+    // CHECK IF THEIR IS USER ID FROM THE REQ
+    const userId = req.user.id;
+    const todos = await getTodoServices(userId);
+
+    // RETURN THE TODO
+    return res.status(200).json({ message: "getting todo", todos });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 // create todos
 export const createTodo = async (req, res) => {
   try {
     const { title, description } = req.body;
+    // USER ID FORM THE PROTECTED ROUTE
+    const userId = req.user.id;
 
     // PASSING THE DESTRUCTURED BODY TO THE CREATE TODO SERVICES
-    const todo = await createTodoServices(title, description);
+    const todo = await createTodoServices(title, description, userId);
 
     // CHECK IF NO TODO
     if (!todo) {
@@ -27,7 +43,22 @@ export const createTodo = async (req, res) => {
 };
 
 // delete todos
-export const deleteTodo = (req, res) => {};
+export const deleteTodo = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const userId = req.user.id;
+
+    const deleteTodo = await deleteTodoServices(id, userId);
+
+    return res.status(200).json({
+      message: "Todo deleted successfully",
+      deleted_todo: deleteTodo.title,
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
 // edit todos
 export const editTodo = (req, res) => {};
