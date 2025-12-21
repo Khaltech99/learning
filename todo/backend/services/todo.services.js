0b926a2856bd18403e1082ff074515ecba1d0c8a
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-12-21 15:43:47 +0100

    Add: Added the get_todo_services for getting todo.

--- todo/backend/services/todo.services.js
+++ todo/backend/services/todo.services.js
@@ -1,6 +1,7 @@
 import Todo from "../models/todo.model.js";
 
-export const createTodoServices = async (title, description) => {
+// CREATE TODO SERVICE
+export const createTodoServices = async (title, description, user) => {
   //   check if the title and description is present
   if (!title || !description) {
     throw new Error("Field cannot be empty");
@@ -9,7 +10,45 @@ export const createTodoServices = async (title, description) => {
   const newTodo = await Todo.create({
     title,
     description,
+    user,
   });
 
   return newTodo;
+};
+
+// GET TODO SERVICE
+export const getTodoServices = async (userId) => {
+  if (!userId || userId === null || userId === "undefined") {
+    throw new Error("YOU ARE NOT AUTHORIZED TO DO THIS");
+  }
+  const todos = await Todo.find({ user: userId });
+  if (todos.length === 0) {
+    throw new Error("YOU DO NOT HAVE ANY TODOS AT THE MOMENT");
+  }
+
+  return todos;
+};
+
+// DELETE TODO SERVICE
+export const deleteTodoServices = async (todoId, userId) => {
+  // CHECK IF TODO EXISTS
+  if (!todoId) {
+    throw new Error("TODO WITH THIS ID NOT FOUND");
+  }
+
+  // CHECK IF USER EXISTS
+  if (!userId) {
+    throw new Error("USER NOT FOUND");
+  }
+
+  const deletedTodo = await Todo.findOneAndDelete({
+    _id: todoId,
+    user: userId,
+  });
+
+  if (!deletedTodo) {
+    throw new Error("ERROR DELETING TODO");
+  }
+
+  return deletedTodo;
 };
