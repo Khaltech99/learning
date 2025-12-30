8233d4bbd0f0a59359ac36348b7ac5fec6c3737e
Khaltech99 <olatunjibabatundejunior@gmail.com>
2025-12-30 21:32:46 +0100

    Add: Implement error handling middleware and custom error utility; enhance user registration and login validation; implement rate limiting for API requests; update todo model and services for improved functionality.

--- todo/backend/app.js
+++ todo/backend/app.js
@@ -1,6 +1,8 @@
-import express, { urlencoded } from "express";
 import router from "./routes/todo_route/todo.route.js";
 import authRouter from "./routes/auth_route/auth.route.js";
+import { errorHandler } from "./middlewares/ErrorHandler.js";
+import express from "express";
+import { customError } from "./utils/error.js";
 
 const app = express();
 
@@ -10,5 +12,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", router);
 app.use("/auth", authRouter);
+
+// 404 handler - should catch all unmatched routes
+app.use((req, res, next) => {
+  const error = customError("The requested resource was not found", 404);
+  res.json({ message: error.message });
+  next(error);
+});
+
+app.use(errorHandler);
 
 export default app;
